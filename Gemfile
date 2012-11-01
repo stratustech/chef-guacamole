fb52d7abc03bcf84e50308e4d042b461796ce8ec
jdutton <jeff.dutton@stratus.com>
2012-11-01 17:36:11 -0400

    Add initial test-kitchen support, though it's not yet passing

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "http://rubygems.org"
 
-gem 'berkshelf', :path => "../berkshelf"
+gem "berkshelf", "= 0.6.0.beta4"
+gem "foodcritic"
+gem "test-kitchen"
--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,6 @@ description      "Installs/Configures guacamole"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+supports "ubuntu"
+
 depends "tomcat"
