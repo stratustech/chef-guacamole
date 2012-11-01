129acfbe98dff74d14222d9848b244c96668dfc1
jdutton <jeff.dutton@stratus.com>
2012-11-01 12:02:06 -0400

    Skeleton for guacamole cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "guacamole"
+maintainer       "Stratus Technologies"
+maintainer_email "jeff.dutton@stratus.com"
+license          "Apache 2.0"
+description      "Installs/Configures guacamole"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
